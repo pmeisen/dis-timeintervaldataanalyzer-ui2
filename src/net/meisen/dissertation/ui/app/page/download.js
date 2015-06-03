68ac949c40f0f5f55470a6b52ea957baa5668e52
philipp <philipp@meisen.net>
2015-06-03 15:54:07 +0200

    - first version of download

--- src/net/meisen/dissertation/ui/app/page/download.js
+++ src/net/meisen/dissertation/ui/app/page/download.js
@@ -2,12 +2,9 @@ define(['jquery', 'net/meisen/dissertation/ui/app/utilities'], function ($, util
 
   return function () {
 
-    // fix the download link if not on webpage
-    if (!util.isWebsite()) {
-      var $hrefDownload = $('#hrefDownload');
-      $hrefDownload.attr('href', 'http://tida.meisen.net/' + $hrefDownload.attr('href'));
-    }
-
-
+    $('#btnDownload').click(function() {
+      var url = (util.isWebsite() ? '' : 'http://tida.meisen.net/') + 'downloads/tidais-TRUNK-SNAPSHOT.zip';
+      window.location.href = url;
+    });
   };
 });
